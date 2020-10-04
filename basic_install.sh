ef91fec6cc9901dad91823d8427f9e9782aa1a1b
ofeks10 <ofeks10@gmail.com>
2020-10-04 15:10:52 +0300

    added web dev stuff

--- basic_install.sh
+++ basic_install.sh
@@ -7,9 +7,15 @@ sudo apt install -y python3-pip
 # Install and configure powerline-shell
 pip3 install powerline-shell
 cp -r ./.config/powerline-shell ~/.config
+sudo apt install -y fonts-powerline
+echo "Don't forget to update your terminal font"
 
 # Install vscode
 sudo snap install --classic code
 
 # Make bash case-insensitive
 cp ./dotfiles/.inputrc ~/.inputrc
+
+# Web dev stuff
+# install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
